8db392aed1ceea66eab71c963302d51b6e101f14
bique14 <peerasorn.h@kbtg.tech>
2023-11-01 23:48:13 +0700

    feat: add env

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,19 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-}
+  build: {
+    rollupOptions: {
+      output: {
+        globals: {
+          VITE_ENDPOINT_URL: process.env.VITE_ENDPOINT_URL,
+        },
+      },
+    },
+  },
+};
